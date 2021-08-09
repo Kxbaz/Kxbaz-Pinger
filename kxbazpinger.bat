839d758fa1db708ec10310d1b25cd4e579c92860
Kxbaz <87565919+Kxbaz@users.noreply.github.com>
2021-08-09 17:48:36 +0200

    Pingeur
    Voici mon pingeur, pas grand chose a dire dessus.

---
+++ kxbazpinger.bat
@@ -0,0 +1,585 @@
+@echo off
+title kxbaz pinger 
+mode 60,20
+color 1
+
+echo P
+echo  I
+echo   N
+echo    G
+echo     E
+echo      R
+
+echo      .
+echo       B
+echo        Y
+echo          . 
+echo       K
+echo      X
+echo       B
+echo      A
+echo       Z
+
+pause
+
+set /p IP = ENTRE L'IP DU BATARD:
+:rainbow
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color a
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color b
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color c
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color d
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color e
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color f
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 1
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 2
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 3
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 4
+PING -n 1 %IP%  | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo hors-ligne...)
+ping -t 2 0 10 127.0.0.1 >nul
